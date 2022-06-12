1e6a9ed1a6f5b5345bb2ddc10002225b854cde03
ClickNinYT <74685931+ClickNinYT@users.noreply.github.com>
2022-06-12 14:07:35 +0700

    Improvements and fixes

--- verify.py
+++ verify.py
@@ -80,6 +80,7 @@ async def verify(ctx: interactions.CommandContext, token: str = None):
     await ctx.defer()
     user = ctx.author
     dm = ctx.member
+    member = ctx.member
     global api_key
     if ctx.author.roles and verified_role_id in ctx.author.roles:
         await ctx.send(str(user) + " has been already verified!")
@@ -87,31 +88,37 @@ async def verify(ctx: interactions.CommandContext, token: str = None):
     else:
         if not api_key:
             await dm.send("No API keys available for verification! Please contact the server\'s owner immediately.")
-            await ctx.send("Sent user " + str(user) + " a DM!")
+            await ctx.send("User " + str(user) + " please check your DM!")
             return
         else:
             if not token:
                 token = PolymartAPI.generateVerifyURL()
                 await dm.send("**Please login into your Polymart account, then visit this link to get your token. After that run this command again with the token argument to continue the verification process!** \n\n" + token)
-                await ctx.send("Sent user " + str(user) + " a DM with getting started instructions!")
+                await ctx.send("User " + str(user) + " please check your DM for instructions to get started!")
             else:
                 user_id = PolymartAPI.verifyUser(token)
                 user_data = PolymartAPI.getUserData(api_key, user_id)
                 resource_user_data = PolymartAPI.getResourceUserData(api_key, resource_id, user_id)
                 username = user_data['response']['user']['username']
                 purchaseValid = resource_user_data['response']['resource']['purchaseValid']
-                if purchaseValid == "false":
-                    await ctx.send("Polymart Verification Summary for " + str(ctx.author) + "\n\nUsername: " + username + "\nUser ID: " + user_id + "\nVerification Status: Unverified \n\n\nResult: Oraxen ownership not found. If you have your license in Spigot instead, contact the server\'s moderators to transfer your license to Polymart (if possible). After that, run this command again to get verified!")
+                if not purchaseValid:
+                    embed = interactions.Embed(title="Verification Summary for user " + str(ctx.author), description="Username: " + username + "\nUser ID: " + user_id + "\nStatus: Unverified \n\nResult: You didn\'t buy Oraxen on Polymart yet. If you have a license on Spigot instead, you may contact the server\'s moderators to assist in transfering your license to Polymart. After that try verify again! \n\n**IF YOU THINK THIS IS DONE BY ERROR, CONTACT THE SERVER\'S OWNER FOR SUPPORT!**", color=0xab2033)
+                    embed.set_footer(text="Verification For User " + str(ctx.author) + " at " + str(datetime.now().strftime("%m-%d-%Y %H:%M:%S")), icon_url=member.user.avatar_url)
+                    await ctx.send(embeds=embed)
                     return
                 else:
                     purchaseStatus = resource_user_data['response']['resource']['purchaseStatus']
                     if purchaseStatus == "Manual" or purchaseStatus == "Imported":
                         await user.add_role(verified_role_id, server_id)
-                        await ctx.send("Polymart Verification Summary for " + str(ctx.author) + "\n\nUsername: " + username + "\nUser ID: " + user_id + "\nVerification Status: Verified \n\n\nResult: You got added to buyer list of the plugin manually. Verified successfully!")
+                        embed = interactions.Embed(title="Verification Summary for user " + str(ctx.author), description="Username: " + username + "\nUser ID: " + user_id + "\nStatus: Verified \n\nResult: You got added to buyer list of the plugin manually. Verified successfully!", color=0x33a343)
+                        embed.set_footer(text="Verification For User " + str(ctx.author) + " at " + str(datetime.now().strftime("%m-%d-%Y %H:%M:%S")), icon_url=member.user.avatar_url)
+                        await ctx.send(embeds=embed)
                         return
                     else:
                         await user.add_role(verified_role_id, server_id)
-                        await ctx.send("Polymart Verification Summary for " + str(ctx.author) + "\n\nUsername: " + username + "\nUser ID: " + user_id + "\nVerification Status: Verified \nPayPal Status Code: " + purchaseStatus + "\n\n\nResult: You have bought the plugin using Paypal. Verified successfully!")
+                        embed = interactions.Embed(title="Verification Summary for user " + str(ctx.author), description="Username: " + username + "\nUser ID: " + user_id + "\nStatus: Verified \nPaypal Status Code: " + purchaseStatus + "\n\nResult: You have bought the plugin using Paypal. Verified successfully!", color=0x33a343)
+                        embed.set_footer(text="Verification For User " + str(ctx.author) + " at " + str(datetime.now().strftime("%m-%d-%Y %H:%M:%S")), icon_url=member.user.avatar_url)
+                        await ctx.send(embeds=embed)
                         return
 
 @bot.command(
@@ -169,6 +176,7 @@ async def del_api_key(ctx: interactions.CommandContext):
             elif os.path.exists("api_key.dev") and api_key is not None:
                 os.remove("api_key.dev")
                 await ctx.send("The API key has been hardcoded to the bot's source code, so the bot has blocked this action. Please contact the server\'s owner if you think this is done by error.", ephemeral=True) 
+    
 
 Init.api_key_init()
 bot.start()
